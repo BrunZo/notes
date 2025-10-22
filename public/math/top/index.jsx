d6e9678b10773d1e9dfed48aedb5e5c4b7830de9
Bruno Ziger <59898191+BrunZo@users.noreply.github.com>
2025-10-21 23:25:15 -0300

    feat: better notes structure (#8)
    * chore: update zettel
    
    * feat: moved some notes and added plenty of topology notes

--- public/math/top/index.jsx
+++ public/math/top/index.jsx
@@ -1,13 +1,16 @@
-import { Zettels } from "zettel/lib/proxy";
+import ZettelList from "zettel/ui/zettel_list";
+import { filterZettels } from "zettel/lib/retrieve";
 
 export const id = "topology";
 export const title = "Topology";
+export const abstract = "Topology is the study of spaces and their properties.";
+export const tags = ["math", "topology"];
+export const date = new Date("2025-10-21 16:20:00");
 
-export default function Topology() {
-    return (
-        <div>
-            <Zettels.topological_space mode="semi-full" showTitle={true} />
-            <Zettels.hausdorff_axiom mode="semi-full" showTitle={true} />
-        </div>
-    )
+export default async function Topology() {
+    const zettels = await filterZettels({
+        globPattern: "math/top/**/*.mdx",
+        sortMethod: (a, b) => (a.sectionNumber || 0) - (b.sectionNumber || 0)
+    });
+    return <ZettelList zettels={zettels} />;
 }
