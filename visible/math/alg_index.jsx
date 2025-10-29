8a0804ea355ab0bafe42ff7cad1a01644eb0b317
BrunZo <ziger.bruno@gmail.com>
2025-10-28 23:52:11 -0300

    add: alg notes & fixed top notes names

---
+++ visible/math/alg_index.jsx
@@ -0,0 +1,16 @@
+import ZettelList from "zettel/ui/z/zettel_list";
+import { filterZettels } from "zettel/lib/retrieve";
+
+export const id = "algebra";
+export const title = "Algebra";
+export const abstract = "Algebra is the study of equations and their relations.";
+export const tags = ["math", "algebra"];
+export const date = new Date();
+
+export default async function Topology() {
+    const zettels = await filterZettels({
+        globPattern: "hidden/math/alg/**/*.mdx",
+        sortMethod: (a, b) => (a.sectionNumber || 0) - (b.sectionNumber || 0)
+    });
+    return <ZettelList zettels={zettels} />;
+}
